d9d996bee9d5ead3587746bfaf7a403f22aa0468
Casey1995 <54296795+Casey1995@users.noreply.github.com>
2021-07-07 22:49:57 -0400

    initial commit

--- web.py
+++ web.py
@@ -4,14 +4,21 @@ span = []
 def intAsignment():
     try:
         url = input("Enter your url: ")
+        
+        #Validating the user entered url.
         validators.url(url)
         while not validators.url(url):
             print('Please enter a valid url')
             url = input("Enter your url: ")
+            
+        #Fetching/reading the contents of the url.
         request = requests.get(url)
         served = request.text
+        #creating a file rock.txt, and writing the scrapped content from the web(url) to it.
         with open ("rock.txt", "w") as output:
             output.writelines(served)
+
+        #Reading/parsing the file to get ingredients
         with open ("rock.txt", "r") as recipe:
             for lines in recipe.readlines():
                 if 'span class="description"' in lines:
